6056879e79bfc060875605e4aed254d4840ccfc9
jonicuber <45526033+jonicuber@users.noreply.github.com>
2020-02-04 13:10:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,44 +1,77 @@
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>PHP Application - AWS Elastic Beanstalk</title>
-    <meta name="viewport" content="width=device-width">
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lobster+Two" type="text/css">
-    <link rel="icon" href="https://awsmedia.s3.amazonaws.com/favicon.ico" type="image/ico" >
-    <link rel="shortcut icon" href="https://awsmedia.s3.amazonaws.com/favicon.ico" type="image/ico" >
-    <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-    <link rel="stylesheet" href="/styles.css" type="text/css">
-</head>
-<body>
-    <section class="congratulations">
-        <h1><font color="red">Congratulations Boys!</h1>
-        <h2><font color="green">USA-BROOKLYN-NYC!</h2>
-        <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
-        <p>You are running PHP version <?= phpversion() ?></p>
-        </section>
+<HTML>
+<HEAD>
+<TITLE>Ivan D Home Page!</TITLE>
+<META NAME="description" CONTENT="Ivan D Home Page!">
+<META NAME="keywords" CONTENT="ANDESA, Demo, Intro, Fido,">
 
-    <section class="instructions">
-        <h2>What's Next?</h2>
-        <ul>
-            <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/">AWS Elastic Beanstalk overview</a></li>
-            <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/create_deploy_PHP_eb.html">Deploying AWS Elastic Beanstalk Applications in PHP Using Eb and Git</a></li>
-            <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/create_deploy_PHP.rds.html">Using Amazon RDS with PHP</a>
-            <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/customize-containers-ec2.html">Customizing the Software on EC2 Instances</a></li>
-            <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/customize-containers-resources.html">Customizing Environment Resources</a></li>
-        </ul>
+<SCRIPT LANGUAGE="JavaScript">
+      var sizes = new Array(0,1,2,4,8);
+      sizes.pos = 0;
+    
+function Elastic()
+{
+    var el = document.all.elastic
+    if (null == el.direction)el.direction = 1
+    else if ((sizes.pos > sizes.length - 2) || (0 == sizes.pos))
+    el.direction *= -1
+    el.style.letterSpacing = sizes[sizes.pos += el.direction]
+setTimeout('Elastic()',100)
+}
+</SCRIPT>
 
-        <h2>AWS SDK for PHP abd for kids</h2>
-        <ul>
-            <li><a href="http://aws.amazon.com/sdkforphp">AWS SDK for PHP home</a></li>
-            <li><a href="http://aws.amazon.com/php">PHP developer center</a></li>
-            <li><a href="https://github.com/aws/aws-sdk-php">AWS SDK for PHP on GitHub</a></li>
-        </ul>
-        <h2>Some Pictures</h2>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/c/cf/Mars%2C_Earth_size_comparison.jpg" alt="Marsik" wight="128" height="128" /> 
-        <img src="https://www.techdonut.co.uk/sites/default/files/how-to-find-the-best-cloud-services-for-your-business-285307991.jpg" alt="Marsik2" wight="128" height="128" />           
-    </section>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-    <!--[if lt IE 9]><script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script><![endif]-->
+  ga('create', 'UA-97865635-1', 'auto');
+  ga('send', 'pageview');
+</script>
+
+<BODY  background="green"  onLoad=Elastic()>
+
+<CENTER>
+<font color="red">
+<H1 ID="elastic" ALIGN="Center">Welcome To Ivan's D Home Page!</H1>
+<font size=4 color=black>
+<TABLE BORDER=solid CELLPADDING=5  style="background-color:rgba(0, 0, 0, 0)">
+
+<CAPTION><font size=+2><font color="green">Last Updated: 8 October 2019</CAPTION>
+
+<td><font color="gold" size=+2>My Curriculum Vitae</td><td><center><a href="https://photocars.s3.us-east-2.amazonaws.com/Ivan-D.docx"><font color="darkblue" size=+2>ID.doc<a/></td><tr>
+
+<td><font color="gold" size=+2>Linkedin Profile</td><td><font color="lime"><center><a href="https://www.linkedin.com/in/ivan-d-762375183/" target="_blank">  
+          <img src="http://pastoralcounseling.com/wp-content/uploads/2016/06/linkedin-badge-300x100.png" width="160" height="50" border="0" alt="View Ivan Dyriv profile on LinkedIn"></a></td><tr>
+
+<td><font color="gold" size=+2>My Hobby Channel "Diving"</td><td><font color="lime"><center><a href="https://youtu.be/_gHWUid6Z60" target="_blank1">  
+          <img src="https://www.lifewire.com/thmb/921Lq5wGTi7bGs6aQnZTKreeab4=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/YouTube_logo_2015.svg-57ebbd433df78c690fc6ffa0.png" width="160" height="40" border="0" alt="View Ivan Dyriv profile on you tube"></a></td><tr>
+
+<td><font color="gold" size=+2>Electronics and Electricity Book</td><td><center><a href="https://photocars.s3.us-east-2.amazonaws.com/Teach_Yourself_Electicity_and_Electronics%2C_3rd_Edition.pdf"><font color="darkblue" size=+2>Teach Yourself<a/></td><tr>
+
+<td><font color="gold" size=+2>My Favorite Birds<br>
+<font color="white" size=+1>Green Violetear Colibri Thalassinus Poster, 12 beats per second,<br>They are the smallest of birds, most species measuring 7.5 - 13 cm</td>
+<td><img src="https://render.fineartamerica.com/images/rendered/default/poster/8/10/break/images-medium-5/1-green-violetear-colibri-thalassinus-panoramic-images.jpg" width="256" height="200" align="right">
+<img src="http://colinmichaelisphotography.com/wp-content/themes/elitist/framework/timthumb.php?src=https://colinmichaelisphotography.com/wp-content/uploads/2013/03/Colibri-Color-7.jpg&w=1170" width="256" height="200" align="right">
+</td><tr>
+
+<td th colspan="2"><font color="gold" size=+2>
+My Favorite Cars!  <a href="https://iktransportation.com/" target="_blank" >Transport any vehicle click here</a><br>
+<font color="white" size=+1>
+<img src="https://photocars.s3.us-east-2.amazonaws.com/IMG_2453.jpeg" width="256" height="200" align="right">
+
+
+
+<center>
+</td><tr>
+</TABLE>
+<div id="contact">
+          <a href="mailto:cuberjoni@gmail.com">Email Me<img src="https://daveberesford.co.uk/wp-content/uploads/2017/09/email-app-Featured-Image-350x257.png" alt="Email Me" width="90" height="70" /></a>
+          </div>
+          
+<CENTER>
+
 </body>
-</html>
+<a>&copy;2019 All Rights Reserved
+
+</HTML>
